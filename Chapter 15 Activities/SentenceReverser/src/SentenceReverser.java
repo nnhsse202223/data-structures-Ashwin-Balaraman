6fd5704bc6ec961457c72f2b87a70178d7f5ced0
abalaraman2@stu.naperville203.org <89261886+Ashwin-Balaraman@users.noreply.github.com>
2022-09-23 10:48:07 -0500

    Im better

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -15,8 +15,31 @@ public class SentenceReverser
     public static String reverse(String sentence)
     {
         // Complete this method. Use a Stack.
-        ...
-
+        Stack<String> sentences = new Stack<>();
+        Scanner in = new Scanner(sentence);
+        String delimeterString = "[\s]";
+        in.useDelimiter(delimeterString);
+        int i = 0;
+        while (i == 1)
+        {
+            if (in.next().contains("."))
+            {
+                i = 1;
+            }
+            else 
+            {
+                sentences.push(in.next)
+            }
+        }
+        System.out.println(sentences);
+        int index = 0;
+        
+        String rever = "";
+        while (sentences.size() > 0)
+        {
+            rever += " " + sentences.pop();
+        }
+        return rever;
 
 
 
--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,5 +10,18 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+         //create a priority queue of todo items
+         Queue<WorkOrder> g = new PriorityQueue<>();
+         g.add(new WorkOrder(3, "vacuum"));
+         g.add(new WorkOrder(2, "water plants"));
+         g.add(new WorkOrder(2, "make dinner"));
+         g.add(new WorkOrder(2,  "walk dog"));
+         g.add(new WorkOrder(9, "play video games"));
+         g.add(new WorkOrder(1, "take Chapter 15 quiz"));
+
+         while (g.size() > 0)
+         {
+            System.out.println(g.remove()); 
+         }
     }
 }
